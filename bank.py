c428fa4ab11ee0b084fbbb8a78a6fefafd635715
Saodat N <saodatsaydirasulova2@gmail.com>
2023-06-08 11:53:54 -0400

    Added a new file

---
+++ bank.py
@@ -0,0 +1,5 @@
+class Bank:
+
+    def __init__(self, personal_information, account_nummber) -> None:
+        self.personal_information = personal_information
+        self.account_number = account_nummber
