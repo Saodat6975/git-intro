dbdb99b77ee7dd929f702a06db4af20c604fdb70
Saodat N <saodatsaydirasulova2@gmail.com>
2023-06-08 14:13:51 -0400

    created object

--- bank.py
+++ bank.py
@@ -3,3 +3,8 @@ class Bank:
     def __init__(self, personal_information, account_nummber) -> None:
         self.personal_information = personal_information
         self.account_number = account_nummber
+
+
+Saodat = Bank('Saodat Saydirasulova', 138273928)
+
+print(Saodat.account_number)
