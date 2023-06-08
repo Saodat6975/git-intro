9dd3eb05f029cc44a8c9de083c559ead5491a5c9
Saodat N <saodatsaydirasulova2@gmail.com>
2023-06-07 20:08:43 -0400

    Added constructor method

--- pylesson.py
+++ pylesson.py
@@ -1,3 +1,10 @@
-class dog:
+class Human:
     barks = True
-    legs = 4
+    legs = 2
+    arms = 2
+    fingers = 10 
+
+    def __init__(self, old, name, nationality) -> None:
+        self.old = old
+        self.name = name
+        self.nationality = nationality
